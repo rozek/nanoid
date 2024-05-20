70d00dc52dcf5ad72cee9507a4e0e0bc21634a6b
Andreas Rozek <A.Rozek@gmx.de>
2024-05-20 08:24:09 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,4 @@ export function random(bytes: any): any;
 export function customRandom(alphabet: any, defaultSize: any, getRandom: any): (size?: any) => string;
 export function customAlphabet(alphabet: any, size?: number): (size?: any) => string;
 export function nanoid(size?: number): string;
-export { urlAlphabet };
-import { urlAlphabet } from './url-alphabet/index.js';
+export { urlAlphabet } from "./url-alphabet/index.js";
